1c1bf2d1ff4e5614029a1fa04ee71169b39eda11
yliu-eis <49169312+yliu-eis@users.noreply.github.com>
2019-09-09 17:23:21 -0400

    Update Parsing_data.py
    change to get all available days of data from past 30 days

--- Parsing_data.py
+++ Parsing_data.py
@@ -7,17 +7,17 @@ host = "10.81.97.126"
 port = 9200
 final_scroll_data_all=pd.DataFrame([])#used to collect search query from each month
 
-import datetime
-now = datetime.datetime.now()#input the current month of the date from system
+date_list=[]#record all available days of index
+for i in range(len(c)):
+    j=c.iloc[i][0].split(' ')[5]
+    if 'txn-30-day-' in j:
+        date_list.append(j)
+date_list.sort()#sort the list by date
+print('days of data available:', len(date_list))
 
-if now.month<10:#format month to match with ELK index
-    m=str(0)+str(now.month)
-
-for i in range(1, 31):#day 1 to day 31 for this month
-    if i <10:
-        i=str(0)+str(i)
-    print(i)
-    index = str("txn-30-day-2019.")+m+'.'+str(i)
+for i in range(len(date_list)):#all days available in past month
+    index= date_list[i]
+    print(index)
 
     es = ES(host=host, port=port, timeout=100)
 
@@ -44,11 +44,6 @@ for i in range(1, 31):#day 1 to day 31 for this month
                 }    
                }
              ],
-             ##2:
-             "must_not": [
-               { "match": {"details.search_intelligence_info.auto_correct_info.confidence": "Rejected" }},
-               { "match": {"details.search_intelligence_info.auto_correct_info.confidence": "NoSuggestion" }}
-             ],
              ##3:
              "should" : [
              { "term" : { "details.requestor_info.interface_id" : "eds" }},
@@ -68,13 +63,19 @@ for i in range(1, 31):#day 1 to day 31 for this month
         sid = each_scroll['_scroll_id']
         scroll_size = len(each_scroll['hits']['hits'])
         
+        #cid
+        cid=pd.Series(each_scroll['hits']['hits']).apply(lambda x: x['_source']['cid'].split(' ')[0])
+        #user_strategy
+        user_strategy=pd.Series(each_scroll['hits']['hits']).apply(lambda x: x['_source']['details']['search_intelligence_info']['auto_correct_info']['user_strategy'])
+        #confidence
+        confidence=pd.Series(each_scroll['hits']['hits']).apply(lambda x: x['_source']['details']['search_intelligence_info']['auto_correct_info']['confidence'])
         #get user entered search query
         raw_query=pd.Series(each_scroll['hits']['hits']).apply(lambda x: x['_source']['details']['search_intelligence_info']['qualified_term'])
         #get suggested search query
         corrected_query=pd.Series(each_scroll['hits']['hits']).apply(lambda x: x['_source']['details']['search_intelligence_info']['auto_correct_info']['suggestion'])
         #combine them
-        df=pd.concat([raw_query, corrected_query], axis=1)
-        df.columns=['raw_query', 'corrected_query']
+        df=pd.concat([cid, user_strategy, confidence, raw_query, corrected_query], axis=1)
+        df.columns=['cid', 'user_strategy', 'confidence', 'raw_query', 'corrected_query']
         final_scroll_data=pd.concat([final_scroll_data, df]).drop_duplicates()#unique search query
     final_scroll_data_all=pd.concat([final_scroll_data_all, final_scroll_data])
         
